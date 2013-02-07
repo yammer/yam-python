8a9eb29e65bbeffd219d4a3d23770ba4046a7f4f
George Brocklehurst <george.brocklehurst@gmail.com>
2013-02-07 11:02:26 +0100

    Improve FakeYammerServer, and extract to support
    The main improvement is that the run_as_process method will no longer return
    before the server is accepting requests. This removes a race condition and makes
    the tests much more stable.

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -2,43 +2,11 @@
 Tests for excercising the whole system in concert.
 """
 
-from multiprocessing import Process
-from unittest import TestCase
-
-from flask import Flask, request
-
+from support.integration import TestCaseWithFakeYammerServer
 import yampy
 
 
-class FakeYammerServer(object):
-    def __init__(self):
-        self._server = Flask("FakeYammerServer")
-
-        @self._server.route("/oauth2/access_token.json")
-        def access_token():
-            return """{"access_token": {"token": "valid_token"}}"""
-
-        @self._server.route("/api/v1/messages.json")
-        def messages():
-            return """{"messages": [{}, {}, {}]}"""
-
-    def run_as_process(self):
-        self.process = Process(target=self._server.run)
-        self.process.start()
-
-    def stop_process(self):
-        self.process.terminate()
-        self.process.join()
-
-
-class MessageIntegrationTest(TestCase):
-    def setUp(self):
-        self.fake_yammer_server = FakeYammerServer()
-        self.fake_yammer_server.run_as_process()
-
-    def tearDown(self):
-        self.fake_yammer_server.stop_process()
-
+class MessageIntegrationTest(TestCaseWithFakeYammerServer):
     def test_fetching_messages(self):
         authenticator = yampy.Authenticator(
             client_id="valid_client_id",
---
+++ tests/support/integration.py
@@ -0,0 +1,115 @@
+"""
+Shared support for integration tests.
+"""
+
+import json
+from multiprocessing import Process
+import requests
+from requests.exceptions import RequestException
+from unittest import TestCase
+
+import flask
+
+
+class FakeYammerServer(object):
+    """
+    A Flask-based web server that simulates the Yammer API by returning
+    static JSON data that has a similar structure to the real API's responses.
+
+    The server runs on port 5000.
+    """
+
+    def __init__(self):
+        """
+        Creates the server and defines the various routes.
+        """
+        self._server = flask.Flask("FakeYammerServer")
+
+        @self._server.route("/up")
+        def up():
+            """
+            Not part of the Yammer API: This route is polled by the main
+            process to determine when the server process is ready.
+            """
+            return "Server is up"
+
+        @self._server.route("/oauth2/access_token.json")
+        def access_token():
+            return """{"access_token": {"token": "valid_token"}}"""
+
+        @self._server.route("/api/v1/messages.json", methods=["GET"])
+        def messages():
+            return self._message_list_json(
+                count=3,
+                first_id=1,
+            )
+
+        @self._server.route("/api/v1/messages.json", methods=["POST"])
+        def post_message():
+            return self._message_list_json(
+                count=1,
+                first_id=4,
+                body=flask.request.args.get("body"),
+            )
+
+    def run_as_process(self):
+        """
+        Spawns the fake Yammer API server in a new process. Does not return
+        until the server is ready to handle requests.
+        """
+        self.process = Process(target=self._server.run)
+        self.process.start()
+        self._poll_until_server_responds()
+
+    def stop_process(self):
+        """
+        Stops the fake Yammer API server's process.
+        """
+        if hasattr(self, "process"):
+            self.process.terminate()
+            self.process.join()
+
+    def _message_list_json(self, count=1, first_id=1, body="Hello world"):
+        id_range = xrange(first_id, first_id + count)
+        messages = [self._message_dict(msg_id, body) for msg_id in id_range]
+        return json.dumps({
+            "messages": messages,
+            "references": [],
+            "meta": [],
+        })
+
+    def _message_dict(self, msg_id, body):
+        return {
+            "id": msg_id,
+            "body": {
+                "plain": body,
+                "parsed": body,
+                "rich": body,
+            }
+        }
+
+    def _poll_until_server_responds(self):
+        try:
+            requests.get("http://localhost:5000/up", timeout=0.1)
+        except RequestException:
+            self._poll_until_server_responds()
+
+
+class TestCaseWithFakeYammerServer(TestCase):
+    """
+    A TestCase that sets up and tears down a FakeYammerServer.
+
+    Subclasses that need to define their own setUp and tearDown methods
+    must call the superclass implementations.
+    """
+    def setUp(self):
+        try:
+            self._fake_yammer_server = FakeYammerServer()
+            self._fake_yammer_server.run_as_process()
+        except:
+            if hasattr(self, "_fake_yammer_server"):
+                self._fake_yammer_server.stop()
+            raise
+
+    def tearDown(self):
+        self._fake_yammer_server.stop_process()
