7df5c01fa4941592a99e40ae418c9f00a2f0f13d
George Brocklehurst <george.brocklehurst@gmail.com>
2013-02-07 11:05:04 +0100

    Split up auth and message integration tests.

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -6,8 +6,8 @@ from support.integration import TestCaseWithFakeYammerServer
 import yampy
 
 
-class MessageIntegrationTest(TestCaseWithFakeYammerServer):
-    def test_fetching_messages(self):
+class AuthenticationIntegrationTest(TestCaseWithFakeYammerServer):
+    def test_authentication(self):
         authenticator = yampy.Authenticator(
             client_id="valid_client_id",
             client_secret="valid_client_secret",
@@ -15,9 +15,13 @@ class MessageIntegrationTest(TestCaseWithFakeYammerServer):
         )
 
         access_token = authenticator.fetch_access_token("valid_code")
+        self.assertEqual("valid_token", access_token)
 
+
+class MessageIntegrationTest(TestCaseWithFakeYammerServer):
+    def test_fetching_messages(self):
         yammer = yampy.Yammer(
-            access_token=access_token,
+            access_token="valid_token",
             base_url="http://localhost:5000/api/v1",
         )
         message_result = yammer.messages.all()
