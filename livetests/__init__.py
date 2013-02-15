649c5820570df61c9ff829a17d879091648a04b5
George Brocklehurst <george.brocklehurst@gmail.com>
2013-02-15 14:20:54 +0100

    Add license to the top of every file.

--- livetests/__init__.py
+++ livetests/__init__.py
@@ -1,3 +1,20 @@
+# Copyright (c) Microsoft Corporation
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY
+# IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR
+# PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+#
+# See the Apache Version 2.0 License for specific language governing
+# permissions and limitations under the License.
+
 """
 Tests that run against the live Yammer API. These aren't intended to be run
 frequently during development, they exist to automate the process of checking
